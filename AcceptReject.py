e123dda932d783696635e042dc7a22a29661c389
Craig L Brooks <feanorian@users.noreply.github.com>
2023-02-17 18:16:31 -0600

    Update AcceptReject.py

--- AcceptReject.py
+++ AcceptReject.py
@@ -33,9 +33,10 @@ if __name__ == "__main__":
     reg = int(sys.argv[p+1])
   else:
     reg = 0
-
+  # defines the target function
   def f(x):
       return x**4 - 2*x**3 +1
+  # defines proposal function
   def g(x):
       return np.full(x.shape, 1)
   
@@ -44,26 +45,32 @@ if __name__ == "__main__":
   print(seed)
   print(size)
   print(reg)
-
+  # Defines the range of values on the x-axis between 0 and 1  
   xs = np.linspace(0,1,1000)
-
+  
+  # passes x values into the functions
   y1 = f(xs)
   y2 = g(xs)
+  
+  # plots the functions
   plt.plot(xs, y1, label = "f(x)")
   plt.plot(xs, y2, label = "g(x)")
   plt.xlabel("x")
-
   plt.ylabel("y")
   plt.fill_between(xs, y1, reg, alpha = .2)
   plt.legend()
+  
+  # Performs uniform random sampling for x and y
   x_samp = np.random.uniform(low=0, high=1, size=int(size))
   y_samp = np.random.uniform(low=0, high=1, size=int(size))
   
+  # Conditions for the regions of acceptance/rejection
+    
   if reg == 0:
     accept = (y_samp < f(x_samp)).astype(int)
   else:
     accept = (y_samp > f(x_samp)).astype(int)
-  
+  # Plots the sampled points
   plt.scatter(x_samp, y_samp, c=accept, cmap='RdYlGn')
   plt.text(.2, 1.75, f'Sample Size {str(size)}' + '\n' + f'Efficency: {accept.mean():.2f}')
   plt.xlim(0, 1)
